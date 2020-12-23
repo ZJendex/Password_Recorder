4829298d8360120525e47314be986dc48ac95b55
Jiangyifei Zhu <47931455+ZJendex@users.noreply.github.com>
2020-12-23 12:39:22 -0500

    Add project files.

---
+++ savePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Password_Recorder
+{
+    /// <summary>
+    /// Interaction logic for savePage.xaml
+    /// </summary>
+    public partial class savePage : Page
+    {
+        public savePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ searchPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Password_Recorder
+{
+    /// <summary>
+    /// Interaction logic for searchPage.xaml
+    /// </summary>
+    public partial class searchPage : Page
+    {
+        public searchPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            // go to the saving page
+            savePage savePage = new savePage();
+            this.NavigationService.Navigate(savePage);
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            // go to the delete page
+            deletePage deletePage = new deletePage();
+            this.NavigationService.Navigate(deletePage);
+        }
+    }
+}
